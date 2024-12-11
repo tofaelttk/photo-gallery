0f80ac88da7ae1435bfc5fb057daf5c1d8498587
Tofael Hossain <124374109+tofaelttk@users.noreply.github.com>
2024-12-10 23:01:52 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// Optional: Add GSAP animations
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.from("header", { duration: 1, y: -100, opacity: 0 });
+  gsap.from("#gallery h1", { duration: 1, x: -200, opacity: 0, delay: 0.5 });
+});
 
